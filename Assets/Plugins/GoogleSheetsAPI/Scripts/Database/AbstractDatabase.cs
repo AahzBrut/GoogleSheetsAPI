48ee1feb1a5a1e0270e0f9704f6e0057b94988da
AahzBrut <partyomenko@gmail.com>
2021-09-26 12:52:56 +0300

    Fix default sheet name

--- Assets/Plugins/GoogleSheetsAPI/Scripts/Database/AbstractDatabase.cs
+++ Assets/Plugins/GoogleSheetsAPI/Scripts/Database/AbstractDatabase.cs
@@ -7,7 +7,7 @@ namespace GoogleSheetsAPI.Database
     {
         [SerializeField] protected T data;
 
-        public virtual string AssociatedSheet => "1ouhJ3FYE8pfKD2we2LTrRA908t9IbcELPwcn4-EKslI";
+        public virtual string AssociatedSheet => "";
         public abstract string AssociatedWorksheet { get; }
 
         public T Data => data;
